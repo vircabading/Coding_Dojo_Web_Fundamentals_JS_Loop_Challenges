f9492ad176c8f009c72adb5eb2ece6526499ce3c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-16 06:19:57 -0700

    add function to get multiples of 3 from start

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -23,7 +23,20 @@ function get_odd_numbers(start, end) {                  // Function to get all t
     return content;
 }
 
+function get_decreasing_multiples_of_3(start) {         // Function to get all the multiples of 3 in decreasing order from start 
+    var content = [];
+    for (var idx=start; idx >=0; idx--) {
+        if (idx % 3 == 0) {
+            content.push(idx);
+        }
+    }
+    return content;
+}
+
 //// MAIN EXECUTABLE SECTION //////////////////////
 
 log_intro ("1. Print odds 1-20");
 console.log("The odd numbers from 1-20 are: " + get_odd_numbers(1, 20));
+
+log_intro ("2. Decreasing Multiples of 3");
+console.log("The decreasing multiplses of 3 fromm 100 to 0 are: " + get_decreasing_multiples_of_3(100));
