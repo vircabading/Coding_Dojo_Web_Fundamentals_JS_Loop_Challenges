cc4ab04643690754906748ef767cca68a616b1ff
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-16 06:41:06 -0700

    add print the sequence

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -7,9 +7,13 @@
 
 //// FUNCTIONS SECTION ///////////////////////////
 
-function log_intro(description) {                       // Function to print desctions onto the console
+function print_intro(description) {                     // Function to print descriptions onto the console
     console.log ("//////////////////////////////////////////////////")
     console.log(description);
+    console.log("");
+}
+
+function print_ln() {                                    // Function to print an empty line 
     console.log("");
 }
 
@@ -32,11 +36,25 @@ function get_decreasing_multiples_of_3(start) {         // Function to get all t
     }
     return content;
 }
+                                                        // Function to get decreasing numbers from start to end with a give interval 
+function get_numbers_decreasing_order(start, end, interval) {
+    var content = [];
+    for (var idx=start; idx >= end; idx-=interval) {
+        content.push(idx);
+    }
+    return content;
+}
 
 //// MAIN EXECUTABLE SECTION //////////////////////
 
-log_intro ("1. Print odds 1-20");
+print_intro ("1. Print odds 1-20");                     // 1st Loop Challenge
 console.log("The odd numbers from 1-20 are: " + get_odd_numbers(1, 20));
+print_ln();
 
-log_intro ("2. Decreasing Multiples of 3");
+print_intro ("2. Decreasing Multiples of 3");           // 2nd Loop Challenge
 console.log("The decreasing multiplses of 3 fromm 100 to 0 are: " + get_decreasing_multiples_of_3(100));
+print_ln();
+
+print_intro("3. Print the sequence");                   // 3rd loop challenge
+console.log("The numbers from 4 to -3.5 in intervals of 1.5 are: " + get_numbers_decreasing_order(4, -3.5, 1.5));
+print_ln();
